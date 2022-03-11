83f4d373086b9b51456fc1ba7d901109a9fc248e
voolocaru catadenis <volocaru.catalin17@gmail.com>
2022-03-11 18:43:28 +0000

    Try vesion with application properties

--- src/main/java/com/studentrecord/StudentRecordApplication.java
+++ src/main/java/com/studentrecord/StudentRecordApplication.java
@@ -20,22 +20,22 @@ public class StudentRecordApplication {
         SpringApplication.run(StudentRecordApplication.class, args);
     }
 
-//    @Bean
-//    public CorsFilter corsFilter(){
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.setAllowedOrigins(Arrays.asList("https://student-report-frontend-app.herokuapp.com"));
-//        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin" , "Access-Control-Allow-Origin"
-//                , "Content-Type", "Accept","Authorization",
-//                "Origin", "Accept","X-Request-With",
-//                "Access-Control-Request-Method",
-//                "Access-Control-Request-Headers"));
-//        corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type",
-//                "Accept","Authorization",
-//                "Access-Control-Allow-Origin" ,"Access-Control-Allow-Credentials"));
-//        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
-//        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-//        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**" , corsConfiguration);
-//        return new CorsFilter(urlBasedCorsConfigurationSource);
-//    }
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://student-report-frontend-app.herokuapp.com"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin" , "Access-Control-Allow-Origin"
+                , "Content-Type", "Accept","Authorization",
+                "Origin", "Accept","X-Request-With",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type",
+                "Accept","Authorization",
+                "Access-Control-Allow-Origin" ,"Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**" , corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
